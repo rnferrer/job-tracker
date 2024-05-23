d1e5c606881cb0fb2b093a1bba844b04f42e88e0
Raymond Ferrer <rnferrer@ucdavis.edu>
2024-05-23 14:13:27 -0700

    converted components from nav to tabs components

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -3,7 +3,7 @@
 const HomePage = () => {
   return(
     <div>
-      Applied
+      Home
     </div>
   )
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,6 +6,7 @@ import {
   NavigationMenuList, 
   navigationMenuTriggerStyle 
 } from "@/components/ui/navigation-menu";
+import { Tabs } from "@/components/ui/tabs";
 
 import {
   SignedIn,
@@ -13,6 +14,8 @@ import {
   SignInButton,
   UserButton
 } from "@clerk/nextjs"
+import { TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import Link from "next/link";
 import { useState } from "react";
 
 type DashboardRoutes = 'Applied' | 'Saved' | 'Interviews'
@@ -26,30 +29,25 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
   }
 
   return (
-    <div className="flex flex-col w-screen p-20">
+    <div className="flex flex-col w-screen p-20 relative">
       <h1 className="font-bold text-5xl">Job Application Tracker</h1>
         {/* Mount the UserButton component */}
-        <UserButton />
-      <nav className="mt-[3rem]">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()} onClick={()=>handleClick('Applied')}>
-                Applied
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()} onClick={()=>handleClick('Saved')}>
-                Saved Jobs
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()} onClick={()=>handleClick('Interviews')}>
-                Interviews
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="absolute top-5 right-5">
+          <UserButton />
+        </div>
+      <nav className="mt-[2.5rem]">
+        <Tabs className="w-[1000px]">
+          <TabsList className="grid w-[50%] grid-cols-4">
+            <TabsTrigger value="home">Home</TabsTrigger>
+            <TabsTrigger value="applied">Applied</TabsTrigger>
+            <TabsTrigger value="saved">Saved Jobs</TabsTrigger>
+            <TabsTrigger value="interviews">Interviews</TabsTrigger>
+          </TabsList>
+          <TabsContent value="home">home</TabsContent>
+          <TabsContent value="applied">hello</TabsContent>
+          <TabsContent value="saved">saved</TabsContent>
+          <TabsContent value="interviews">intervews</TabsContent>
+        </Tabs>
       </nav>
  
       {children}
