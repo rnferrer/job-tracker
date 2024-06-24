fda14104df917f5b9c83017b6008c0052ac051dc
Raymond Ferrer <rnferrer@ucdavis.edu>
2024-06-24 14:48:37 -0700

    added a status field to the form with select component

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
 
 const applicationSchema = z.object({
   title: z.string()
@@ -19,26 +20,27 @@ const applicationSchema = z.object({
       message: "Company name must not be longer than 50 characters.",
     }),
   link: z.string()
-    .url(
-      { message: "Invalid url" 
-
-      }),
+    .url({ 
+      message: "Invalid url" 
+    }),
   location: z.string()  
     .min(2,{
       message: "Location must be at least 2 characters.",
     }).max(50, {
       message: "Location must not be longer than 50 characters.",
     }),
+  status: z.string()
 })
 
 const ApplicationForm = () => {
   const form = useForm<z.infer<typeof applicationSchema>>({
     resolver: zodResolver(applicationSchema),
     defaultValues:{
-      title:null,
+      title:"",
       company:"",
       link:"",
-      location:""
+      location:"",
+      status: "Applied"
     }
   })
   function onSubmit(values: z.infer<typeof applicationSchema>){
@@ -93,6 +95,29 @@ const ApplicationForm = () => {
                 </FormControl>
               </FormItem>
           )}/>
+          <FormField
+            control={form.control}
+            name="status"
+            render={({field})=>(
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Set status of application"/>
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Applied">Applied</SelectItem>
+                    <SelectItem value="Interview">Interview</SelectItem>
+                    <SelectItem value="NoResponse">No Response</SelectItem>
+                    <SelectItem value="Offer">Offer</SelectItem>
+                    <SelectItem value="Rejected">Rejected</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
           <Button type="submit" className="mt-5">Add Application</Button>
         </form>
       </Form>
