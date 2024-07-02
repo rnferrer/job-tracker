2488bec1dbd8c0028bb326db7c22d62c25f50788
Raymond Ferrer <rnferrer@ucdavis.edu>
2024-07-01 21:42:26 -0700

    Created DataTable component for generalized tables for Applied and Saved page

---
+++ src/components/DataTable.tsx
@@ -0,0 +1,38 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>){
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+})
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
