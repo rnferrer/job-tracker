074db49aeaa157abdc0e633b156220da2ce75372
Raymond Ferrer <rnferrer@ucdavis.edu>
2024-11-06 20:39:30 -0800

    Created base for dropdown menu interactions

--- src/components/applied/EditApplicationMenu.tsx
+++ src/components/applied/EditApplicationMenu.tsx
@@ -12,10 +12,25 @@ import { MoreHorizontal } from "lucide-react"
 const EditApplicationMenu = () => {
   return(
     <>
-      <Button variant="ghost" className="h-8 w-8 p-0">
-        <span className="sr-only">Open menu</span>
-        <MoreHorizontal className="h-4 w-4" />
-      </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem className="cursor-pointer">
+          Add Interview
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer">
+          Edit
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer">
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
     </>
   )
 }
